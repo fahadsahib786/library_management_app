fc74c4ebc1df7c13ce8aa5845ceec4242ad4cd31
Muhammad Fahad <fahadsahib786@gmail.com>
2022-04-25 14:36:36 +0500

    updates

--- test/Library.js
+++ test/Library.js
@@ -1,3 +1,18 @@
 const {expect} = require("chai");
 const {ethers} = required("hardhat");
 
+describe("Library Contract", function()){
+    let Library;
+    let library;
+    let owner;
+
+    const NUM_UNFINISHED_BOOK = 5;
+    const NUM_FINISHED_BOOK = 3;
+
+    let unfinishedBookList;
+    let finishedBookList;
+
+    beforeEach(async function(){
+        
+    })
+}
